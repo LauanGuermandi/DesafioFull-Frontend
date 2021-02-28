f9154609208f76493aabb6ad91a17e33dbd3c201
Lauan Guermandi <lauanguermandi@gmail.com>
2021-02-28 10:24:21 -0300

    feat: adicao do component para criacao de pessoa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { TemplateComponent } from './components/template/template.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ModalAddPessoaComponent } from './components/modal-add-pessoa/modal-add-pessoa.component';
 import { PessoaComponent } from './components/pessoa/pessoa.component';
 
-
+const maskConfig: Partial<IConfig> = {
+  validation: false,
+};
 
 @NgModule({
   declarations: [
     AppComponent,
+    ModalAddPessoaComponent,
     TemplateComponent,
     PessoaComponent
   ],
@@ -20,6 +27,9 @@ import { PessoaComponent } from './components/pessoa/pessoa.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ModalModule.forRoot(),
+    NgxMaskModule.forRoot(maskConfig),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/modal-add-pessoa/modal-add-pessoa.component.ts
@@ -0,0 +1,54 @@
+import {Router} from "@angular/router"
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
+import { Pessoa } from 'src/app/models/pessoa.model';
+import { PessoaService } from './../../services/pessoa.service';
+
+@Component({
+  selector: 'app-modal-add-pessoa',
+  templateUrl: './modal-add-pessoa.component.html',
+  styleUrls: [ './modal-add-pessoa.component.css' ]
+})
+export class ModalAddPessoaComponent implements OnInit {
+  modalRef: BsModalRef;
+  pessoaForm: FormGroup;
+
+  constructor(
+    private modalService: BsModalService,
+    private formBuilder: FormBuilder,
+    private pessoaService: PessoaService,
+    private router: Router
+    ) {
+    this.createForm();
+  }
+
+  ngOnInit() {}
+
+  openModalWithClass(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(
+      template,
+      Object.assign({}, { class: 'gray' })
+    );
+  }
+
+  createForm() {
+    this.pessoaForm = this.formBuilder.group({
+      nome: ['', Validators.required ],
+      cpf: ['', Validators.required ],
+   });
+  }
+
+  addPessoa() {
+    console.log(this.pessoaForm.value)
+    this.pessoaService.post(this.pessoaForm.value).subscribe(
+      (response: Pessoa) => {
+        this.router.navigate([`/dividas/${response.id}`]);
+      },
+      (erro: any) => {
+        console.error(erro);
+      }
+    );
+  }
+}
